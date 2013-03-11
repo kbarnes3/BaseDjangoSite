3328c32fe937dbf1ed3ebd01d318b88ef2cc4466
Kevin Barnes <kbarnes3@gmail.com>
2013-03-10 18:41:25 -0700

    Using different db usernames for different deployments

--- web/newdjangosite/settings_dev.py
+++ web/newdjangosite/settings_dev.py
@@ -9,7 +9,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'newdjangosite-dev',                        # Or path to database file if using sqlite3.
-        'USER': 'newdjangosite-user',
+        'USER': 'newdjangosite-dev-user',
         'PASSWORD': 'passwordgoeshere',
         'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
