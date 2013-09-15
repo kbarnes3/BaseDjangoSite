0fe92788ae5464c88b04aee7a26e375bdc0c1031
Kevin Barnes <kbarnes3@gmail.com>
2013-09-14 17:05:31 -0700

    Fab setup_server works

--- web/fabric_utils/setup.py
+++ web/fabric_utils/setup.py
@@ -1,4 +1,4 @@
-from fabric.api import sudo
+from fabric.api import run, sudo
 
 
 def setup_server():
@@ -13,4 +13,23 @@ def setup_server():
     ]
 
     for package in packages:
+        pass
         sudo('apt-get install --yes {0}'.format(package))
+
+    username = run('echo $USER')
+
+    sudo('addgroup webadmin')
+    sudo('adduser {0} webadmin'.format(username))
+
+    sudo('mkdir /var/www')
+    sudo('mkdir /var/www/python')
+    sudo('chgrp -R webadmin /var/www')
+    sudo('chmod -R ug+w /var/www')
+
+    sudo('createuser -E -P -s {0}'.format(username), user='postgres')
+    run('createuser -s root')
+
+    sudo('mkdir /var/fastcgi')
+    sudo('chmod 777 /var/fastcgi')
+    sudo('rm /etc/nginx/sites-enabled/default')
+    sudo('/etc/init.d/nginx start')
