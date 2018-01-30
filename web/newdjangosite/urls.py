29a1eeda7c18f7975270d70bfda8feb311e56689
Kevin Barnes <kbarnes3@gmail.com>
2018-01-29 18:02:51 -0800

    Updating URLs

--- web/newdjangosite/urls.py
+++ web/newdjangosite/urls.py
@@ -1,21 +1,28 @@
-from django.urls import path, include
+"""NewDjangoSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
+from django.urls import path
 
 from common.views import hello_world
-
-admin.autodiscover()
+import users
 
 urlpatterns = [
-    url(r'^$', hello_world, name='hello_world'),
-    url(r'^users/', include('users.urls')),
+    path('', hello_world, name='hello_world'),
+    path('users/', users.urls),
 
-    # Examples:
-    # url(r'^$', 'newdjangosite.views.home', name='home'),
-    # url(r'^newdjangosite/', include('newdjangosite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
+
