07130cf3b9d7432dcd151c9f67dd6b7294c083a0
Kevin Barnes <kbarnes3@gmail.com>
2018-01-31 17:40:00 -0800

    Fixing up URLs more

--- web/newdjangosite/urls.py
+++ web/newdjangosite/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from common.views import hello_world
-import users
 
 urlpatterns = [
     path('', hello_world, name='hello_world'),
-    path('users/', users.urls),
+    path('users/', include('users.urls')),
 
     path('admin/', admin.site.urls),
 ]
