721d545096ad70204cac2fd051150a90c57804b0
Kevin Barnes <kbarnes3@gmail.com>
2018-03-26 22:19:57 -0700

    Password changing now uses all stock stuff

--- web/users/urls.py
+++ web/users/urls.py
@@ -7,9 +7,6 @@ from users.views import create_user_account
 
 urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='landing_page'), name='logout'),
-    path('password-change/', PasswordChangeView.as_view(
-        template_name='users/password_change.html',
-        success_url='/')),
     path('password_reset/', PasswordResetView.as_view(form_class=EmailPasswordResetForm), name='password_reset'),
     path('', include('django.contrib.auth.urls')),
     path('signup/', create_user_account, name='create_user_account')
