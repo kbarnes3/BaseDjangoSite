684b96027b2a14ad5c8d6c6bcadf7f6625d8899a
Kevin Barnes <kbarnes3@gmail.com>
2018-08-28 18:07:17 -0700

    Adding Update script

--- scripts/Console.ps1
+++ scripts/Console.ps1
@@ -1,9 +1,15 @@
 # Launch a console for the project.
+param(
+    [switch]$Verbose
+)
 
 $project_root = Split-Path $PSScriptRoot
 
 $venv = Join-Path $project_root "venv\scripts\Activate.ps1"
-if (-Not (Test-Path $venv)) {
+if (Test-Path $venv) {
+    . $PSScriptRoot\Update.ps1 -Verbose:$Verbose
+}
+else {
     . $PSScriptRoot\Setup.ps1
 }
 
--- scripts/Update.ps1
+++ scripts/Update.ps1
@@ -1,4 +1,28 @@
 # This script updates the project to run for its current checkout.
+param(
+    [switch]$Verbose
+)
 
-$project_root = Split-Path $PSScriptRoot
+. $PSScriptRoot\Write-Status.ps1
+$already_activated = . $PSScriptRoot\Ensure-Venv.ps1
 
+# Check Python version
+$venv_version = & python --version
+$installed_version = & py -3.6 --version
+if ($venv_version -ne $installed_version) {
+    Write-Status "Updating venv from $venv_version to $installed_version"
+    deactivate
+    $project_root = Split-Path $PSScriptRoot
+    $venv = Join-Path $project_root "venv"
+    & py -3.6 -m venv $venv --upgrade
+    . $PSScriptRoot\Ensure-Venv.ps1 | Out-Null
+}
+
+. $PSScriptRoot\Bootstrap.ps1 -Verbose:$Verbose
+
+Write-Status "Performing database migrations"
+. $PSScriptRoot\Invoke-Manage.ps1 migrate
+
+if (-Not $already_activated) {
+    deactivate
+}
