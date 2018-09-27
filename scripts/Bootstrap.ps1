199d55ccab849eb3c131bf813ae929133e1c7c5b
Kevin Barnes <kbarnes3@gmail.com>
2018-09-26 21:41:03 -0700

    Making scripts a little more predictable

--- scripts/Bootstrap.ps1
+++ scripts/Bootstrap.ps1
@@ -15,12 +15,16 @@ else {
 $project_root = Split-Path $PSScriptRoot
 $already_activated = . $PSScriptRoot\Ensure-Venv.ps1
 
+Push-Location $project_root
+
 Write-Status "Updating pip"
 & python -m pip install --upgrade pip $quiet
 Write-Status "Updating requirements"
 & pip install -r (Join-Path $project_root "requirements.txt") $quiet
 Write-Status "Updating dev-requirements"
 & pip install -r (Join-Path $project_root "dev-requirements.txt") $quiet
+
+Pop-Location
 
 if (-Not $already_activated) {
     deactivate
