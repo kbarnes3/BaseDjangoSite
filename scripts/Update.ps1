9eede5c4beb153921214a5e0df5e413765562596
Kevin Barnes <kbarnes3@gmail.com>
2018-11-04 17:57:47 -0800

    Update can now handle creating a database

--- scripts/Update.ps1
+++ scripts/Update.ps1
@@ -4,6 +4,7 @@ param(
 )
 
 . $PSScriptRoot\Write-Status.ps1
+$project_root = Split-Path $PSScriptRoot
 $already_activated = . $PSScriptRoot\Ensure-Venv.ps1
 
 # Check Python version
@@ -12,7 +13,6 @@ $installed_version = & py -3.6 --version
 if ($venv_version -ne $installed_version) {
     Write-Status "Updating venv from $venv_version to $installed_version"
     deactivate
-    $project_root = Split-Path $PSScriptRoot
     $venv = Join-Path $project_root "venv"
     & py -3.6 -m venv $venv --upgrade
     . $PSScriptRoot\Ensure-Venv.ps1 | Out-Null
@@ -20,8 +20,18 @@ if ($venv_version -ne $installed_version) {
 
 . $PSScriptRoot\Bootstrap.ps1 -Verbose:$Verbose
 
-Write-Status "Performing database migrations"
-. $PSScriptRoot\Invoke-Manage.ps1 migrate
+$local_db = Join-Path $project_root "web\newdjangosite.db"
+if (Test-Path $local_db) {
+    Write-Status "Performing database migrations"
+    . $PSScriptRoot\Invoke-Manage.ps1 migrate
+}
+else {
+    Write-Status "Creating local database"
+    & $PSScriptRoot\Invoke-Manage.ps1 migrate
+
+    Write-Status "Creating super user"
+    & $PSScriptRoot\Invoke-Manage.ps1 createsuperuser
+}
 
 if (-Not $already_activated) {
     deactivate
